1621518503162e9e8ea341a75ee115730ed12551
Juseok <108967786+Jugeok@users.noreply.github.com>
2022-08-15 22:50:30 +0900

    미들웨어추가

---
+++ sever/middleware/loginCheck.js
@@ -0,0 +1,13 @@
+module.exports = (req, res, next) => {
+    
+    //로그인 체크
+    const cookie = req.cookies;
+    if (cookie.Bearer) {
+        res.status(400).send({
+            errorMessage: "이미 로그인 되어 있습니다. ",
+        });
+        return;
+    } else{
+        next();
+    };
+};
