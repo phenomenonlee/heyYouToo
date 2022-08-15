9625710e9946fa96cce94963cc12ce586bb0c42f
Juseok <108967786+Jugeok@users.noreply.github.com>
2022-08-15 16:39:08 +0900

    123

--- sever/middleware/auth-middleware.js
+++ sever/middleware/auth-middleware.js
@@ -1,18 +1,14 @@
 const jwt = require("jsonwebtoken");
 const {User} = require("../models");
 module.exports = (req, res, next)=> {
-    const {authorization} = req.headers;
+    const cookies = req.cookies;
     
-    const [tokenType, tokenValue] = authorization.split(' ');
-    
-    
-    if (tokenType !== 'Bearer') {
-        res.status(401).send ({
-        errorMessage: '로그인 후 사용하세요.',
+    if(!cookies.token){
+        res.status(401).send({
+            errorMessage:'로그인 후 사용하세요.'
         });
         return;
-
-    }
+    };
     
     try {
         
