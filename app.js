906a6543ba844e72a8c67eeb90d540753efdb8b1
Juseok <108967786+Jugeok@users.noreply.github.com>
2022-08-16 17:54:09 +0900

    12415

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
+//설치한 express 모듈 가져오기
 const express = require("express");
+//설치한 cookie-parser 모듈 가져오기
 const cookieParser = require("cookie-parser");
+//설치한 socket io 모듈 가져오기
+const socket = require("socket.io")
+//내장된 http 모듈 가져오기
+const http = require("http")
+//express 객체 생성
 const app = express();
-const port = 3000;
+//express http 서버 생성
+const server = http.createServer(app)
+//생성된 서버를 socket.io에 바인딩
+const io = socket(server)
+//GET방식으로 경로에 접속하면 실행됨
+app.get('/', function(req, res) {
+    console.log("유저가 /으로 접속하였습니다!")
+    response.send("hello, Express Server!!")
+});
+// const port = 3000;
+server.listen(3000, function(){
+    console.log('서버 실행 중..')
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const router = require("./sever/routes");
+const { response } = require("express");
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", router);
+//app.use("/", router);
 
-app.listen(port, () => {
-    console.log(port, "포트로 서버가 열렸어요!");
-});
+// app.listen(port, () => {
+//     console.log(port, "포트로 서버가 열렸어요!");
+// });
--- sever/middleware/loginCheck.js
+++ sever/middleware/loginCheck.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
     
     //로그인 체크
     const cookie = req.cookies;
-    if (cookie.Bearer) {
+    if (cookie.token) {
         res.status(400).send({
             errorMessage: "이미 로그인 되어 있습니다. ",
         });
